4d5d102bddee324d76d8015d357ee998f9f021c3
BZyMek <anperkus@poczta.onet.pl>
2022-10-19 17:03:47 +0200

    add new test

--- cypress/e2e/restful_booker_e2e.cy.js
+++ cypress/e2e/restful_booker_e2e.cy.js
@@ -1,7 +1,23 @@
 describe('Home Page Testing', () => {
-
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
+    beforeEach(() => {
+      cy.visit('https://automationintesting.online/')
+      cy.contains('Let me hack!').click()
     })
-})
-    
+  
+    it('verifies description', () => {          //later add verification of the whole page layout
+  
+        cy.get('.col-sm-10').should('contain', 'Shady Meadows')
+    })
+  
+  
+  
+    it('books a room positive test', () => {
+  
+      cy.get('[class="row hotel-room-info"]')
+        .get('[class="col-sm-7"]')
+        .find('button')
+        .should('contain', 'Book this room')
+        .click()
+  
+    })
+  })
